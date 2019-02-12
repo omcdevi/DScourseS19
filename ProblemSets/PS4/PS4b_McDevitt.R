b1e4926518b14bbea5ed80a4e7799a3de00caab2
Owen McDevitt <omcdevitt1022@ou.edu>
2019-02-12 13:21:43 -0600

    completed PS4

---
+++ ProblemSets/PS4/PS4b_McDevitt.R
@@ -0,0 +1,12 @@
+
+library(datasets)
+df1 <- as.data.frame(iris)
+df <- createDataFrame(iris)
+class(df1)
+class(df)
+head(select(df,df$Sepal_length,df$Species))
+head(filter(df,df$Sepal_length>5.5))
+head(select(df,df$Sepal_length,df$Species),filter(df,df$Sepal_length>5.5))
+head(summarize(groupBy(df, df$Species), mean=mean(df$Sepal_Length), count=n(df$Sepal_Length)))
+df2 <- head(summarize(groupBy(df, df$Species), mean=mean(df$Sepal_Length), count=n(df$Sepal_Length)))
+head(arrange(df2, asc(df2$Species)))
